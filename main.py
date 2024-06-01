353675c934560a1828fe27ff079a4d84431749ce
dkadrienn <72705062+dkadrienn@users.noreply.github.com>
2024-06-01 20:04:02 +0300

    reorganize

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-# from tensorflow.keras import datasets, layers, models
-import pandas
-
-# (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
-#
-# # normalize data from 0-255 (pixel brightness) to 0-1
-# training_images, testing_images = training_images / 255, testing_images / 255
-#
-# class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
-#
-# for i in range(16):
-#     plt.subplot(4, 4, i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.imshow(training_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(class_names[training_labels[i][0]])
-#
-# plt.show()
-#
-# training_images = training_images[:20000]
-# training_labels = training_labels[:20000]
-# testing_images = testing_images[:4000]
-# testing_labels = testing_labels[:4000]
-#
-# model = models.Sequential()
-# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.Flatten())
-# model.add(layers.Dense(64, activation='relu'))
-# model.add(layers.Dense(10, activation='softmax')) #softmax scales, how likely is the given result
-#
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
+import data_prep
 import init_data
 import resnet50
 
-style_classes = init_data.get_classes('data/wikiart_csv/style_class.txt')
+style_classes_csv = 'data/wikiart_csv/style_class.txt'
 
-model = resnet50.CustomResNet50(style_classes.__len__())
-model.train('data/wikiart_csv/style_train_reduced.csv', 'data/wikiart_csv/style_val_reduced.csv')
-loss, accuracy = model.evaluate('data/wikiart_csv/style_test_reduced.csv')
-print(f'Test accuracy: {accuracy}, test loss: {loss}')
+train_csv = 'data/wikiart_csv/style_train.csv'
+val_test_csv = 'data/wikiart_csv/style_val_test.csv'
+
+train_filtered_csv = 'data/wikiart_csv/style_train_filtered.csv'
+val_test_filtered_csv = 'data/wikiart_csv/style_val_test_filtered.csv'
+
+train_reduced_csv = 'data/wikiart_csv/style_train_reduced.csv'
+val_test_reduced_csv = 'data/wikiart_csv/style_val_test_reduced.csv'
+
+val_reduced_csv = 'data/wikiart_csv/style_val_reduced.csv'
+test_reduced_csv = 'data/wikiart_csv/style_test_reduced.csv'
 
 
+def prepare_data():
+    # filter data
+    data_prep.filter_data(train_csv)
+    data_prep.filter_data(val_test_csv)
 
+    # reduce data
+    # 1/4 of the whole dataset
+    data_prep.reduce_data(train_filtered_csv, 14135)
+    data_prep.reduce_data(val_test_filtered_csv, 6050)
+
+    # split val dataset into val and test
+    data_prep.split_data(val_test_reduced_csv, val_reduced_csv, test_reduced_csv)
+
+
+def main():
+    # filter non acii characters
+    # reduce datasets sizes
+    # split validation data into validation and testing datasets
+    # prepare_data()
+
+    style_classes = init_data.get_classes(style_classes_csv)
+
+    model = resnet50.CustomResNet50(style_classes.__len__())
+    model.train(train_reduced_csv, val_reduced_csv)
+    loss, accuracy = model.evaluate(test_reduced_csv)
+    print(f'Test accuracy: {accuracy}, test loss: {loss}')
+
+
+if __name__ == "__main__":
+    main()
